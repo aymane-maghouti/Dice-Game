1e99cbb31143933943a6eb25e23008add6a52f4a
Aymane maghouti <112390782+aymane-maghouti@users.noreply.github.com>
2024-04-09 17:20:28 +0000

    Update DeconnectServlet.java

--- Dice_Game/src/main/java/com/web/servlets/DeconnectServlet.java
+++ Dice_Game/src/main/java/com/web/servlets/DeconnectServlet.java
@@ -7,11 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-/**
- * 
- * @author Tarik BOUDAA
- *
- */
+
 public class DeconnectServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
